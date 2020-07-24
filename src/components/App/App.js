7a2a142fbfb0916e4c2c897158992bdc4fffa7c7
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-24 18:50:33 -0500

    routes for swatches and colors components created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 // COMPONENTS
 import Header from '../Header/Header';
-
+//Routes
+import Swatches from '../pages/Swatches/Swatches';
+import Colors from '../pages/Colors/Colors';
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
+	render() {
+		return (
+			<Router>
+				<div>
+					<Header />
 
-                <div className="container">
-                    PAGE CONTENT
-                </div>
-            </div>
-        );
-    }
+					<div className='container'>
+						<Route path='/' exact component={Swatches} />
+						<Route path='/' exact component={Colors} />
+					</div>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
