d611d1ec6db978b69566858dd6a77d1511a53a25
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-28 10:45:25 -0500

    Color.js funionality created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 // COMPONENTS
 import Header from '../Header/Header';
--- src/components/pages/Colors/Colors.js
+++ src/components/pages/Colors/Colors.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const mapStoreToProps = (reduxState) => ({
+	reduxState,
+});
 class Colors extends Component {
+	state = {
+		newColor: {
+			label: '',
+			hex_code: '',
+		},
+	};
+	componentDidMount() {
+		this.props.dispatch({
+			type: 'GET_COLORS',
+		});
+	}
+	handleInputChange = (event, inputKey) => {
+		this.setState({
+			newColor: {
+				...this.state.newColor,
+				[inputKey]: event.target.value,
+			},
+		});
+	};
+	addNewColor = (event, inputKey) => {
+		event.preventDefault();
+		this.props.dispatch({
+			type: 'ADD_COLOR',
+			payload: this.state.newColor,
+		});
+		this.setState({
+			newColor: {
+				label: '',
+				hex_code: '',
+			},
+		});
+		this.props.dispatch({
+			type: 'GET_COLORS',
+		});
+	};
 	render() {
-		return <div></div>;
+		const colorList = this.props.reduxState.getColorsReducer.map((item, index) => {
+			const el = `#${item.hex_code}`
+			return (
+				<tbody key={index}>
+					<tr>
+						<td>{item.label}</td>
+						<td>{item.hex_code}</td>
+						<td style={{ backgroundColor: el }}></td>
+						<td><button>Delete</button></td>
+					</tr>
+				</tbody>
+			)
+		})
+		return <div>
+			<h3>Color Settings</h3>
+			<table>
+				<thead>
+					<tr>
+						<th>Label</th>
+						<th>Hex Code</th>
+						<th>Block</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				{colorList}
+			</table>
+		</div>;
 	}
 }
 
-export default Colors;
+export default connect(mapStoreToProps)(Colors);
