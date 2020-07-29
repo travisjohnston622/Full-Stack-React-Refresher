5906eda8ba43f5dfc1800a07700822f52c61afa5
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-29 17:42:51 -0500

    functionality created for swatches.js

--- src/components/pages/Swatches/Swatches.js
+++ src/components/pages/Swatches/Swatches.js
@@ -34,9 +34,34 @@ class Swatches extends Component {
         })
     }
     render() {
+        const colorBlocks = this.props.reduxState.getBlockReducer.map((item, index) => {
+            const el = `#$(item.hex_code)`
+            const block = <div style={{ backgroundColor: el }} className="block"></div>
+            return (
+                <div key={index} className="blockBody">
+                    {block}
+                    <h3 className="label">{item.label}</h3>
+                    <div className="label">
+                        <button onClick={(event) => this.deleteBlock(event, item.id)}>Delete</button>
+                    </div>
+                </div>
+            )
+        })
+        const colorButtons = this.props.reduxState.getColoraReducer.map((item, index) => {
+            const el = `#$(item.hex_code)`
+            return (
+                <div key={index} className="blockBody">
+                    <button className="addColorBtn" style={{ backgroundColor: el }} onCLick={(event) => this.addBlock(event, item)}>
+                        Add a(n) {item.label} box!
+                    </button>
+                </div>
+            )
+        })
         return (
             <div>
-
+                {colorButtons}
+                <h2>Color Swatches</h2>
+                {colorBlocks}
             </div>
         )
     }
