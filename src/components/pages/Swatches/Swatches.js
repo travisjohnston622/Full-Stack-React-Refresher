5a3f5124c8197eb88c45224a2e2087408b516c70
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-29 17:27:20 -0500

    dispatches created in swatches.js

--- src/components/pages/Swatches/Swatches.js
+++ src/components/pages/Swatches/Swatches.js
@@ -6,6 +6,33 @@ const mapStoreToProps = reduxState => ({
 })
 
 class Swatches extends Component {
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'GET_BLOCKS'
+        })
+        this.props.dispatch({
+            type: 'GET_COLORS'
+        })
+    }
+
+    deleteBlock = (event, id) => {
+        this.props.dispatch({
+            type: 'DELETE_BLOCK',
+            payload: id
+        })
+        this.props.dispatch({
+            type: 'GET_BLOCKS',
+        })
+    }
+    addBlock = (event, item) => {
+        this.props.dispatch({
+            type: 'ADD_BLOCK',
+            payload: item
+        })
+        this.props.dispatch({
+            type: 'GET_BLOCKS',
+        })
+    }
     render() {
         return (
             <div>
