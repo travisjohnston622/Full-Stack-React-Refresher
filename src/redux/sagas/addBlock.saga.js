16d451d4bffb4b4a66558380a4792ee16e132d4a
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-01 19:42:30 -0500

    addBlockSaga created

---
+++ src/redux/sagas/addBlock.saga.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function addBlock(action) {
+    try {
+        const response = yield axios({
+            method: 'POST',
+            url: '/api/blocks/',
+            data: action.payload
+        })
+    } catch (err) {
+        console.log('Error with block post', err);
+    }
+}
+function* addBlockSaga() {
+    yield takeLatest('ADD_BLOCK', addBlock);
+}
+
+export default addBlockSaga;
