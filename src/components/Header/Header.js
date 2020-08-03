1169b17c844f291a4cdf5324414646bb11ea0154
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-02 20:26:45 -0500

    updated nav and links in header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css';
-
+//React-Router
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 class Header extends Component {
     render() {
         return (
             <header className="appBar">
                 <h1>Color Swatches</h1>
+                <nav>
+                    <div>
+                        <Link to='/'>Swatches</Link>
+                    </div>
+                    <div>
+                        <Link to='/colors'>Colors</Link>
+                    </div>
+                </nav>
             </header>
         );
     }
