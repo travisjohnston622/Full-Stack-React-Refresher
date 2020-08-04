f906fcdaf0710ad3ef2518ad1a8755c1ba2add2e
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-03 19:16:57 -0500

    routes created in blocks.router.js

--- server/routers/blocks.router.js
+++ server/routers/blocks.router.js
@@ -8,12 +8,41 @@ const pool = require('../modules/pool');
 
 // BASE MODE - make a route to get all saved color blocks
 router.get('/', (req, res) => {
-    // the data from both the "colors" and "blocks" tables will be need
-    res.sendStatus(500);
+    const queryString = `SELECT "blocks".id, "colors".label, "colors".hex_code
+    FROM "blocks"
+    JOIN "colors" ON "blocks".color_id = "colors".id;`;
+    pool.query(queryString)
+        .then((response) => {
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
 });
 
 // BASE MODE - make a route to save new color blocks item
-
+router.post('/', (req, res) => {
+    const newBlockColor = req.body;
+    const queryString = `INSERT INTO "blocks" ("color_id")
+    VALUES ('${newBlockColor.id}');`;
+    pool.query(queryString)
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
 // BASE MODE - make a route to delete a specific color block
-
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const queryString = `DELETE FROM "blocks" WHERE id=$1;`;
+    pool.query(queryString, [id])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
 module.exports = router;
